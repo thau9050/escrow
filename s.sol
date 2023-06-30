df1332f566e5287871ad88f037d367fffce29cc4
thau9050 <131345580+thau9050@users.noreply.github.com>
2023-07-01 00:28:21 +0300

    Update s.sol

--- s.sol
+++ s.sol
@@ -1,1 +1,31 @@
+pragma solidity ^0.8.0;
+
+contract Escrow {
+  address payable public payer;
+  address payable public payee;
+  address public escrowAgent;
+  uint public amount;
+
+  constructor(address payable _payer, address payable _payee, uint _amount) {
+    payer = _payer;
+    payee = _payee;
+    amount = _amount;
+    escrowAgent = msg.sender;
+  }
+
+  function deposit() external payable {
+    require(msg.sender == payer, "Only payer can deposit funds");
+    require(msg.value == amount, "Deposit amount must match agreed upon amount");
+  }
+
+  function release() external {
+    require(msg.sender == escrowAgent, "Only escrow agent can release funds");
+    payable(payee).transfer(address(this).balance);
+  }
+
+  function cancel() external {
+    require(msg.sender == escrowAgent, "Only escrow agent can cancel escrow");
+    payable(payer).transfer(address(this).balance);
+  }
+}
 
